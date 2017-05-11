64b918a363a6451d5da7d5957216e79639e55050
Joshua McCormick <jjm2g13@ecs.soton.ac.uk>
2017-05-11 18:41:59 +0100

    Basic base capture

---
+++ Assets/Scripts/BaseController.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseController : MonoBehaviour {
+
+	public static int secondsToTake = 3; //can make unstatic later if we want
+	private static float refreshRate = (float) secondsToTake / 100;
+
+	GameObject colour;
+	GameObject center;
+
+	//public int baseNumber;
+	//Assume no occupants or team at start
+	private int[] occupants = {0, 0}; //{red, blue}
+	public Team team = Team.NONE;
+	private int percentTaken = 0;
+	private bool changing = false;
+
+	// Use this for initialization
+	void Start () {
+		colour = transform.Find ("Colour").gameObject;
+		center = transform.Find ("Center").gameObject;
+
+		colour.GetComponent <SpriteRenderer>().color = Color.grey;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!changing) {
+			if (team == Team.NONE) {
+				if (occupants [(int)Team.RED] > 0) {
+					team = Team.RED;
+					colour.GetComponent <SpriteRenderer> ().color = Color.red;
+					InvokeRepeating ("IncreaseHold", refreshRate, refreshRate);
+					changing = true;
+				} else if (occupants [(int)Team.BLUE] > 0) {
+					team = Team.BLUE;
+					colour.GetComponent <SpriteRenderer> ().color = Color.blue;
+					InvokeRepeating ("IncreaseHold", refreshRate, refreshRate);
+					changing = true;
+				}
+			} else if (team == Team.RED && occupants [(int)Team.RED] == 0 && occupants [(int)Team.BLUE] > 0) {
+				InvokeRepeating ("DecreaseHold", refreshRate, refreshRate);
+				changing = true;
+			} else if (team == Team.BLUE && occupants [(int)Team.BLUE] == 0 && occupants [(int)Team.RED] > 0) {
+				InvokeRepeating ("DecreaseHold", refreshRate, refreshRate);
+				changing = true;
+			} else if (percentTaken < 100 && occupants [(int)team] > 0) {
+				InvokeRepeating ("IncreaseHold", refreshRate, refreshRate);
+				changing = true;
+			}
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D other){
+		if (other.tag == "Player") {
+			PlayerController player = other.GetComponent <PlayerController> ();
+			occupants [(int) player.team]++;
+		}
+	}
+	
+
+	void OnTriggerExit2D(Collider2D other){
+		if (other.tag == "Player") {
+			PlayerController player = other.GetComponent <PlayerController> ();
+			occupants [(int) player.team]--;
+		}
+	}
+
+	void IncreaseHold(){
+		percentTaken++;
+
+		//If has been fully taken, stop
+		if (percentTaken == 100) { 
+			center.GetComponent <SpriteRenderer>().color = team == Team.RED ? Color.red : Color.blue;
+			CancelInvoke ("IncreaseHold");
+			changing = false;
+		//If no players in team left in area, stop
+		} else if (occupants [(int)team] == 0) { 
+			CancelInvoke ("IncreaseHold");
+			changing = false;
+		}
+
+		//Show percent taken
+		float scale = (float) percentTaken / 100;
+		colour.transform.localScale = new Vector3 (scale, scale, 1);
+	}
+
+	void DecreaseHold(){
+		percentTaken--;
+
+		//Attacking team = team not currently controlling this base
+		Team attackingTeam = team == Team.BLUE ? Team.RED : Team.BLUE;
+
+		//If 0% taken, another team can claim
+		if (percentTaken == 0) {
+			CancelInvoke ("DecreaseHold");
+			team = Team.NONE;
+			center.GetComponent <SpriteRenderer> ().color = Color.grey;
+			changing = false;
+		//If someone from the team returns to the base, they can increase their hold 
+		} else if (occupants [(int)team] > 0) {
+			CancelInvoke ("DecreaseHold");
+			InvokeRepeating ("IncreaseHold", refreshRate, refreshRate);
+			changing = true;
+		//If attacking team leaves, they stop decreasing the current hold
+		} else if (occupants [(int)attackingTeam] == 0) {
+			CancelInvoke ("DecreaseHold");
+			changing = false;
+		}
+
+		//Show percent taken
+		float scale = (float) percentTaken / 100;
+		colour.transform.localScale = new Vector3 (scale, scale, 1);
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public Team team;
+	public float moveSpeed = 100.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	// Update is called once per frame
+	void Update () {
+		float right = 0;
+		float up = 0;
+
+		if (team == Team.RED) {
+			right = moveSpeed * Input.GetAxis ("p1_h");
+			up = moveSpeed * Input.GetAxis ("p1_v");
+		} else if (team == Team.BLUE) {
+			right = moveSpeed * Input.GetAxis ("p2_h");
+			up = moveSpeed * Input.GetAxis ("p2_v");
+		}
+
+		right *= Time.deltaTime;
+		up *= Time.deltaTime;
+
+		transform.Translate (right, up, 0);
+	}
+}
