f2c73a933edcf384d76068e3d137f70dc507ea29
Jianzu Guan <guan-1995@hotmail.com>
2017-05-16 16:26:26 +0100

    fixed UI not hide on start

--- Assets/_Scripts/GameState.cs
+++ Assets/_Scripts/GameState.cs
@@ -24,16 +24,18 @@ public class GameState : MonoBehaviour {
     // Use this for initialization
     void Start() {
         if (countdownUI == null) {
-            countdownUI = GameObject.Find("/Canvas/Countdown");
-            countdownUI.SetActive(true);
+            countdownUI = GameObject.Find("/CanvasGameState/Countdown");
         }
+        countdownUI.SetActive(true);
+
         if (countdownText == null) {
             countdownText = GameObject.Find("/Canvas/Countdown/CountdownText").GetComponent<Text>();
         }
+
         if (endScreen == null) {
-            endScreen = GameObject.Find("/Canvas/EndScreen");
-            endScreen.SetActive(false);
+            endScreen = GameObject.Find("/CanvasGameState/EndScreen");
         }
+        endScreen.SetActive(false);
     }
 
     // Update is called once per frame
